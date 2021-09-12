8431cbf15dfe278fd3282cfd63eaba338fa4a037
Mauriciogen3 <mauriciogen3@hotmail.com>
2021-09-12 15:16:50 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,3 +10,28 @@ button.addEventListener('click', (event) => {
   } else {
     usuario.classList.remove("errorInput")
   }
+
+
+  if (senha.value == '') {
+    senha.classList.add("errorInput")
+  } else {
+    senha.classList.remove("errorInput")
+  }
+
+  if (usuario.value.indexOf("@") == -1 || usuario.value.indexOf(".") == -1 || (usuario.value.indexOf(".") - usuario.value.indexOf("@") == 1)) {
+    usuario.classList.add("errorInput")
+  } else {
+    usuario.classList.remove("errorInput")
+  }
+
+  if (!isNaN(usuario.value) == true && usuario.value.length == 11) {
+    usuario.classList.remove("errorInput")
+  }
+
+  if (senha.value.length <= 5) {
+    senha.classList.add("errorInput")
+  } else {
+    senha.classList.remove("errorInput")
+  }
+
+})
