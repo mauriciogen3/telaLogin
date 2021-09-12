50d315a81722d08db1b6485e61cac3d91dcdf1f4
Mauriciogen3 <mauriciogen3@hotmail.com>
2021-09-12 16:15:16 -0300

    Update scripts.js

---
+++ Cadastro/scripts.js
@@ -0,0 +1,15 @@
+
+let btn = document.querySelector('#verSenha')
+let btnConfirm = document.querySelector('#verConfirmSenha')
+
+let nome = document.querySelector('#nome')
+let labelNome = document.querySelector('#labelNome')
+let validNome = false
+
+let usuario = document.querySelector('#usuario')
+let labelUsuario = document.querySelector('#labelUsuario')
+let validUsuario = false
+
+let senha = document.querySelector('#senha')
+let labelSenha = document.querySelector('#labelSenha')
+let validSenha = false
